aebde13096c6c911698fbb7c245c498e8689b5d5
Amir <amir_tarig@yahoo.com>
2020-09-22 14:19:51 +0200

    testingVue

--- app.js
+++ app.js
@@ -1,37 +1,97 @@
-let app = new Vue({
-  el: "#app",
-  data: {
-    brand: "Vue Mastery",
-    product: "Socks",
-    selectedVariant: 0,
+//Create a new component for product-details with a prop of details.
 
-    inventory: 0,
-    details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    variants: [
-      {
-        variantId: 2234,
-        variantColor: "green",
-        variantQuantity: 0,
-        variantImage:
-          "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
-      },
-
-      {
-        variantId: 2236,
-        variantColor: "blue",
-        variantQuantity: 10,
-        variantImage:
-          "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg",
-      },
-    ],
-    sizes: ["S", "M", "L", "XL", "XXL", "XXXL"],
-    cart: 0,
-    onSale: false,
+Vue.component("product-details", {
+  props: {
+    details: {
+      type: Array,
+      required: true,
+    },
   },
+  template: `
+      <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+      </ul>
+    `,
+});
 
+Vue.component("product", {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  template: `
+     <div class="product">
+          
+        <div class="product-image">
+          <img :src="image" />
+        </div>
+  
+        <div class="product-info">
+            <h1>{{ product }}</h1>
+            <p v-if="inStock">In Stock</p>
+            <p v-else>Out of Stock</p>
+            <p>Shipping: {{ shipping }}</p>
+  
+            <product-details :details="details"></product-details>
+  
+            <div class="color-box"
+                 v-for="(variant, index) in variants" 
+                 :key="variant.variantId"
+                 :style="{ backgroundColor: variant.variantColor }"
+                 @mouseover="updateProduct(index)"
+                 >
+            </div> 
+  
+            <button v-on:click="addToCart" 
+              :disabled="!inStock"
+              :class="{ disabledButton: !inStock }"
+              >
+            Add to cart
+            </button>
+
+  
+         </div>  
+      
+      </div>
+     `,
+  data() {
+    return {
+      product: "Socks",
+      brand: "Vue Mastery",
+      selectedVariant: 0,
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: "green",
+          variantImage:
+            "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+          variantQuantity: 10,
+        },
+        {
+          variantId: 2235,
+          variantColor: "blue",
+          variantImage:
+            "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg",
+          variantQuantity: 0,
+        },
+      ],
+    };
+  },
+  methods: {
+    addToCart: function () {
+      //this.cart += 1;
+      this.$emit("add-to-cart", this.variants[this.selectedVariant].variantId);
+    },
+    updateProduct: function (index) {
+      this.selectedVariant = index;
+    },
+  },
   computed: {
     title() {
-      this.brand + this.product;
+      return this.brand + " " + this.product;
     },
     image() {
       return this.variants[this.selectedVariant].variantImage;
@@ -39,21 +99,24 @@ let app = new Vue({
     inStock() {
       return this.variants[this.selectedVariant].variantQuantity;
     },
-    Sale() {
-      if (this.variants.onSale) {
-        return `${this.brand}  ${this.product}  Currntly On Sale`;
+    shipping() {
+      if (this.premium) {
+        return "Free";
       }
-      return `${this.brand}  ${this.product} Currntly Is Not On Sale`;
+      return 2.99;
     },
   },
+});
 
+var app = new Vue({
+  el: "#app",
+  data: {
+    premium: true,
+    cart: [],
+  },
   methods: {
-    addToCart() {
-      this.cart++;
-    },
-
-    updateProduct(index) {
-      this.selectedVariant = index;
+    updateCart(id) {
+      this.cart.push(id);
     },
   },
 });
